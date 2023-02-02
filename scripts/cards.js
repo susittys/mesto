49dcfd88979e6c83b9d027c83ac26ffe043348a2
Alexandr <91794286+susittys@users.noreply.github.com>
2023-02-02 11:38:40 +0300

    -fix size full-image -move cards array to standalone js -rename some variable -add one closer's func. for all popup -fix button "x" closers type "reset" -fix optimizated some variable -add identific-classes for form -fix submit for form

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+        name: 'Воркута',
+        link: './images/cities/vorkuta.jpg'
+    },
+    {
+        name: 'Сыктывкар',
+        link: './images/cities/syktyvkar.jpg'
+    },
+    {
+        name: 'Вуктыл',
+        link: './images/cities/vuktyl.jpg'
+    },
+    {
+        name: 'Усинск',
+        link: './images/cities/usinsk.jpg'
+    },
+    {
+        name: 'Ухта',
+        link: './images/cities/ukhta.jpeg'
+    },
+    {
+        name: 'Печора',
+        link: './images/cities/pechora.jpg'
+    }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,98 +1,57 @@
-const initialCards = [
-    {
-        name: 'Воркута',
-        link: './images/cities/vorkuta.jpg',
-        descr: 'Город Воркута в республике Коми'
-    },
-    {
-        name: 'Сыктывкар',
-        link: './images/cities/syktyvkar.jpg',
-        descr: 'Город Сыктывкар в республике Коми'
-    },
-    {
-        name: 'Вуктыл',
-        link: './images/cities/vuktyl.jpg',
-        descr: 'Город Вуктыл в республике Коми'
-    },
-    {
-        name: 'Усинск',
-        link: './images/cities/usinsk.jpg',
-        descr: 'Город Усинск в республике Коми'
-    },
-    {
-        name: 'Ухта',
-        link: './images/cities/ukhta.jpeg',
-        descr: 'Город Ухта в республике Коми'
-    },
-    {
-        name: 'Печора',
-        link: './images/cities/pechora.jpg',
-        descr: 'Город Печора в республике Коми'
-    }
-];
-
 // эл-ты Набора карточек
 const elementsContainer = document.querySelector('.elements__collection'),
       elementsNoPlaces  = document.querySelector('.elements__no-places');
 
 // эл-ты блока Профиль
-const editProfile = document.querySelector('.profile__edit'),
+const buttonEditProfile = document.querySelector('.profile__edit'),
+      buttonAddPlace    = document.querySelector('.profile__add'),
       nameProfile = document.querySelector('.profile__name'),
-      jobProfile  = document.querySelector('.profile__job'),
-      addPlace    = document.querySelector('.profile__add');
+      jobProfile  = document.querySelector('.profile__job');
 
 // эл-ты попап-окна Редактирования Профиля
 const popupEditProfile = document.querySelector('.popup_type_profile'),
+      formEditProfile  = popupEditProfile.querySelector('.popup__form'),
       nameEditProfile  = popupEditProfile.querySelector('.popup__input_type_name'),
-      jobEditProfile   = popupEditProfile.querySelector('.popup__input_type_job'),
-      closeEditProfile = popupEditProfile.querySelector('.popup__close_type_profile');
+      jobEditProfile   = popupEditProfile.querySelector('.popup__input_type_job');
 
 // эл-ты попап-окна Добавление места
 const popupAddPlace = document.querySelector('.popup_type_place'),
+      formAddPlace  = popupAddPlace.querySelector('.popup__form'),
       titleAddPlace = popupAddPlace.querySelector('.popup__input_type_title'),
-      linkAddPlace  = popupAddPlace.querySelector('.popup__input_type_link'),
-      closeAddPlace = popupAddPlace.querySelector('.popup__close_type_place');
+      linkAddPlace  = popupAddPlace.querySelector('.popup__input_type_link');
 
 const popupFullImage = document.querySelector('.popup_type_image'),
       imageFullImage = popupFullImage.querySelector('.popup__full-image'),
-      titleFullImage = popupFullImage.querySelector('.popup__subtitle-image'),
-      closeFullImage = popupFullImage.querySelector('.popup__close_type_image');
+      titleFullImage = popupFullImage.querySelector('.popup__subtitle-image');
+
+const buttonCloseList = document.querySelectorAll('.popup__close');
+
+const placeTemplate = document.querySelector('#place-template').content;
 
 
-
-    //####################################//
+//####################################//
    // Попап-окно: Редактирование профиля //
   //####################################//
 
 // обработчик клика кнопки Редактирования Профиля
-editProfile.addEventListener('click', function () {
+buttonEditProfile.addEventListener('click', function () {
     nameEditProfile.value = nameProfile.textContent;
     jobEditProfile.value  = jobProfile.textContent;
-    togglePopup(popupEditProfile);
+    openPopup(popupEditProfile);
 });
 
 // обр-ик клика кн-ки "Сохранить"
-popupEditProfile.addEventListener('submit', submitEditProfile);
-
-// обр-ик клика кнопки X (закрытия)
-closeEditProfile.addEventListener('click', function () {
-    togglePopup(popupEditProfile);
-});
+formEditProfile.addEventListener('submit', submitEditProfile);
 
 // Фн-ия сохранения данных полей формы
 function submitEditProfile(event) {
     event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Так мы можем определить свою логику отправки.
 
-    let nameSaved = nameEditProfile.value,
-        jobSaved  = jobEditProfile.value;
+    nameProfile.textContent = nameEditProfile.value;
+    jobProfile.textContent  =  jobEditProfile.value;
 
-    if ( nameSaved.length && jobSaved.length ) {
-        nameProfile.textContent = nameSaved;
-        jobProfile.textContent  = jobSaved;
-
-        togglePopup(popupEditProfile);
-    }
+    closePopup(popupEditProfile);
 }
 
     //##############################//
@@ -100,20 +59,15 @@ function submitEditProfile(event) {
   //##############################//
 
 // обработчик клика кнопки Добавление места
-addPlace.addEventListener('click', function () {
+buttonAddPlace.addEventListener('click', function () {
     titleAddPlace.value = '';
     linkAddPlace.value  = '';
-    togglePopup(popupAddPlace);
+    openPopup(popupAddPlace);
 });
 // обр-ик клика кн-ки "Сохранить"
 
 // обр-ик клика кн-ки "Создать"
-popupAddPlace.addEventListener('submit', submitAddPlace);
-
-// обр-ик клика кнопки X (закрытия)
-closeAddPlace.addEventListener('click', function () {
-    togglePopup(popupAddPlace);
-});
+formAddPlace.addEventListener('submit', submitAddPlace);
 
 // Фн-ия добавления новой карточки
 function submitAddPlace(event) {
@@ -123,76 +77,61 @@ function submitAddPlace(event) {
     let infoPlace = {};
         infoPlace['name']  = titleAddPlace.value;
         infoPlace['link']  = linkAddPlace.value;
-        infoPlace['descr'] = 'Изображение города России';
 
-    if ( infoPlace['name'] && infoPlace['link'] ) addPlaces( infoPlace );
+    if ( infoPlace['name'] && infoPlace['link'] ) createPlaces( infoPlace );
 
-    togglePopup(popupAddPlace);
+    closePopup(popupAddPlace);
 }
 
-// Фн-ия перебирает набор с городами, в случае инициализации
-// в случае добавления нового, примет массив с одним объектом
-function addPlaces( place = {} ) {
-    if ( Object.entries(place).length ) {
-        const placeTemplate = document.querySelector('#place-template').content,
-              placeElement  = placeTemplate.querySelector('.elements__element').cloneNode(true),
-              placeImageGrp = placeElement.querySelector('.elements__image-group');
-
-        placeElement.querySelector('.elements__title').textContent = place['name'];
-        placeElement.querySelector('.elements__image').src = place['link'];
-        placeElement.querySelector('.elements__image').alt = place['descr'];
-
-        const likeElement = placeElement.querySelector('.elements__like'),
-            removeElement = placeElement.querySelector('.elements__remove');
-
-        // обработчики кликов кнопок на наборе карточек с фото
-        likeElement.addEventListener('click', function (likeBtn) {
-            likeBtn.target.classList.toggle('elements__like_active');
-        });
-
-        removeElement.addEventListener('click', function (removeBtn) {
-            let listElement = removeBtn.target.closest('.elements__element');
-            listElement.remove();
-            checkNoPlaces();
-        });
-
-        placeImageGrp.addEventListener('click', function (imageGroup) {
-            const imageFather = imageGroup.target.closest('.elements__element'),
-                  imageTitle  = imageFather.querySelector('.elements__title').textContent,
-                  imageLink   = imageFather.querySelector('.elements__image').src;
-
-            showFullImage(imageTitle,imageLink);
-        });
-
-        elementsContainer.prepend(placeElement);
-    } else alert('Ошибка: нет названия или ссылки');
+function renderPlaces(placesCard) {
+    elementsContainer.prepend(placesCard);
 
     // если картинка добавлена, убрать "Нет добавленных.."
     checkNoPlaces();
 }
 
+// в случае добавления нового, примет массив с одним объектом
+function createPlaces( place = {} ) {
+    const placeElement  = placeTemplate.querySelector('.elements__element').cloneNode(true),
+          placeImageGrp = placeElement.querySelector('.elements__image-group');
 
+    const buttonLikeElement = placeElement.querySelector('.elements__like'),
+        buttonRemoveElement = placeElement.querySelector('.elements__remove'),
+        titlePlaceElement   = placeElement.querySelector('.elements__title'),
+        imagePlaceElement   = placeElement.querySelector('.elements__image');
+
+    titlePlaceElement.textContent  = place['name'];
+    imagePlaceElement.src = place['link'];
+    imagePlaceElement.alt = place['name'];
+
+    // обработчики кликов кнопок на наборе карточек с фото
+    buttonLikeElement.addEventListener('click', function (likeBtn) {
+        likeBtn.target.classList.toggle('elements__like_active');
+    });
+
+    buttonRemoveElement.addEventListener('click', function (removeBtn) {
+        let listElement = removeBtn.target.closest('.elements__element');
+        listElement.remove();
+        checkNoPlaces();
+    });
+
+    placeImageGrp.addEventListener('click', () => showFullImage(place.name, place.link) );
+
+    renderPlaces(placeElement);
+}
+
+
+    //##################################//
+   // Попап-окно: Просмотр изображения //
   //##################################//
- // Попап-окно: Просмотр изображения //
-//##################################//
-
-// обр-ик клика кнопки X (закрытия)
-closeFullImage.addEventListener('click', function () {
-
-    // при закрытии окна, обнуляется данные формы
-    imageFullImage.src = 'images/image_no-places.jpg';
-    imageFullImage.alt = 'Изображение города';
-    titleFullImage.textContent = 'Описание изображения';
-
-    togglePopup(popupFullImage);
-});
 
 // Фн-ия открытия и загрузки данных в попап
 function showFullImage(titleImage, linkImage) {
     imageFullImage.src = linkImage;
+    imageFullImage.alt = titleImage;
     titleFullImage.textContent = titleImage;
 
-    togglePopup(popupFullImage);
+    openPopup(popupFullImage);
 }
 
 
@@ -201,12 +140,23 @@ function showFullImage(titleImage, linkImage) {
   //###############//
 
 // при инициализации страницы
-initialCards.forEach(addPlaces);
+initialCards.forEach(createPlaces);
 
-// Фн-ия открытия и закрытия Попап
-function togglePopup(elementForm = '') {
-    if ( elementForm ) elementForm.classList.toggle("popup_opened")
-    else alert('Ошибка: не найдена popup-форма');
+// обр-ик клика кнопки X (закрытия)
+
+buttonCloseList.forEach(btn => {
+    const popup = btn.closest('.popup');
+    btn.addEventListener('click', () => closePopup(popup));
+})
+
+// Фн-ия открытия Попап
+function openPopup(elementForm) {
+    elementForm.classList.add("popup_opened")
+}
+
+// Фн-ия закрытия Попап
+function closePopup(elementForm) {
+    elementForm.classList.remove("popup_opened")
 }
 
 //если нет places(карточек), то показать текст "нет добавленных.."
